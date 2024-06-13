8358ed38a87e86419c0dfdaa0a3c9cbca8bc7f87
NicolasGasparetto <167822500+NicolasGasparetto@users.noreply.github.com>
2024-06-13 19:46:52 -0300

    a volta

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    <link type="image/png" sizes="16x16" rel="icon" href="./Gerenciamento-Escolar-PHP/assets/icons8-school-16.png">
+    <link rel="stylesheet" href="./Gerenciamento-Escolar-PHP/View/pags/css/index.css"> 
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="nav-links">
+                <a href="index.php">Home</a>
+                <a href="./Gerenciamento-Escolar-PHP/View/pags/html/sobre.php">Sobre Nós</a>
+                <a href="./Gerenciamento-Escolar-PHP/View/pags/html/faleconosco.php">Fale Conosco</a>
+            </div>
+            <div class="login">
+                <img src="./Gerenciamento-Escolar-PHP/assets/user-icon-svgrepo-com.svg" alt="Login" width="24" height="24">
+                <a href="./Gerenciamento-Escolar-PHP/View/pags/html/login.php">Login</a>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <section class="welcome-section">
+            <h1>Bem-vindo ao Instituto Educacional Nova Era</h1>
+        </section>
+        
+        <section class="education-section">
+            <h2>Tipos de Ensino Oferecidos</h2>
+            <p>Nossa escola oferece uma ampla variedade de modalidades de ensino para atender às necessidades de todos os alunos, incluindo:</p>
+            <ul>
+                <li>Ensino Infantil</li>
+                <li>Ensino Fundamental</li>
+                <li>Ensino Médio</li>
+                <li>Educação de Jovens e Adultos (EJA)</li>
+                <li>Cursos Técnicos</li>
+            </ul>
+        </section>
+
+        <section class="social-section" id="social-section">
+            <h2>Acesse nossas redes sociais!</h2>
+            <ul>
+                <li><a href="#"><img src="./Gerenciamento-Escolar-PHP/assets/facebook-svgrepo-com.svg" alt="Facebook"></a></li>
+                <li><a href="#"><img src="./Gerenciamento-Escolar-PHP/assets/youtube-svgrepo-com.svg" alt="Youtube"></a></li>
+                <li><a href="#"><img src="./Gerenciamento-Escolar-PHP/assets/instagram-svgrepo-com.svg" alt="Instagram"></a></li>
+            </ul>
+        </section>
+    </main>
+    <footer class="footer">
+        <p>&copy; 2024 Instituto Educacional Nova Era. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
